30a25046ed1c33bad8b0f88c620f3fa8fca02af3
simonhenrique <simon_henrique16@outlook.com>
2024-04-16 21:25:27 -0300

    commit

--- VacinaPlus/App.js
+++ VacinaPlus/App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native-paper';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Button icon="camera" mode="contained" onPress={() => console.log('Pressed')}>
+        Press me
+      </Button>
       <StatusBar style="auto" />
     </View>
   );
