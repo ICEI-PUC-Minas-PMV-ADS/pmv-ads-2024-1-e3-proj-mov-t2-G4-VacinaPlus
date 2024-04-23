9c36506430b7f79ae2d3fd98a19c01337c801e8b
Pedro Henrique Gomes de Souza <126032592+PedroHgds@users.noreply.github.com>
2024-04-23 00:19:26 -0300

    Atualização tela home

--- VacinaPlus/src/views/home.js
+++ VacinaPlus/src/views/home.js
@@ -6,51 +6,42 @@ import BarraNavegacao from '../components/BarraNavegacao';
 const { width, height } = Dimensions.get('window');
 
 const TelaHome = () => {
-  
-  const handlePressInicio = () => {
-    console.log('Início pressionado');
-  };
-
-  const handlePressVacinas = () => {
-    console.log('Vacinas pressionado');
-  };
-
-  const handlePressAgenda = () => {
-    console.log('Agenda pressionado');
-  };
-
-  const handlePressPerfil = () => {
-    console.log('Perfil pressionado');
-  };
-
-  const navigation = useNavigation();
-
   return (
     <View style={styles.container}>
-      <ScrollView style={styles.scrollView}>
-        
-        <View style={styles.header}>
+      <ScrollView style={styles.scrollView}>        
+        <View style={styles.header}>       
           <Image source={require('../../assets/logo-plus.png')} style={styles.logo} />
           <Text style={styles.welcome}>Olá, Usuário!</Text>
           <TouchableOpacity style={styles.notificationButton}>
             <Icon name="notifications" size={25} color="#00BFFF" />
           </TouchableOpacity>
         </View>
-        
-        {/* Seção de Imagens/Anúncios */}
+
+      {/* Seção de Imagens */}
+<View style={styles.section}>
+  <Image source={require('../../assets/vacina.jpg')} style={styles.image} />
+  <Text style={styles.sectionText}></Text>
+</View>
+
+
+        {/* Seção de Notícias */}
         <View style={styles.section}>
-          <Image source={{ uri: '../../assets/.jpg' }} style={styles.image} />
-          <Text style={styles.sectionText}>Anúncios</Text>
+        <Image source={require('../../assets/vacinacovid.jpg')} style={styles.image} />
+        <Text style={{ textAlign: 'justify' }}>Para evitar a perda de imunizantes e garantir a vacinação da população, a partir desta sexta-feira (19), a Prefeitura de Belo Horizonte vai concentrar a oferta das vacinas. A doses contra dengue serão ofertadas em 13 postos de saúde e contra a covid-19 em 30 unidades. Os endereços dos locais que vão ofertar os imunizantes em cada regional, por tipo, podem ser verificados on-line.</Text>
         </View>
-        
-        
+
+         <View style={styles.section}>
+         <Image source={require('../../assets/kidvacina.jpg')} style={styles.image} />
+         <Text style={{ textAlign: 'justify' }}>A Prefeitura de Belo Horizonte ampliou os locais de vacinação contra a gripe e covid-19 nas regionais Nordeste e Barreiro. A aplicação das doses na Universo - Campus Belo Horizonte, que fica na Rua Paru, 762 - Nova Floresta, foi iniciada nesta quarta-feira (10). O local vacina o público em dias úteis, das 9h às 12h e das 13h às 16h. Já a partir desta quinta-feira (11) as vacinas serão aplicadas no Via Shopping, na Avenida Afonso Vaz de Melo, 640 - Barreiro, também de segunda a sexta-feira, das 11h às 19h30. Cabe destacar que nesses locais as doses serão administradas exclusivamente no público adulto.
+</Text>
+        </View>
       </ScrollView>
       
       {/* Barra de Navegação com botões*/}
        <BarraNavegacao/>
     </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
